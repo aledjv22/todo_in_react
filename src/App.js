2fba12e16bd18cfb3fafcd1a9ad8b7d52ca0f9c6
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-07-05 21:27:26 -0300

    ToDo's through an arrangement and props

--- src/App.js
+++ src/App.js
@@ -4,22 +4,32 @@ import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
+import React from 'react';
+
+const defaultTodos = [
+  { text: 'Cut onion', completed: true },
+  { text: 'Take the React course', completed: false },
+  { text: 'Cry with La Llorona', completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
 
-      <TodoCounter/>
+      <TodoCounter completed={16} total={25} />
       <TodoSearch/>
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => 
+          <TodoItem 
+          key={todo.text} 
+          text={todo.text}
+          completed={todo.completed}/>
+          )}
       </TodoList>
 
       <CreateTodoButton/>
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,7 +1,7 @@
-function TodoCounter(){
+function TodoCounter({ total, completed }){
     return (
       <h1>
-        You have completed 3 of 5 TODOs
+        You have completed {completed} of {total} TODOs
         </h1>
     );
 }
