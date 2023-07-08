0a84452d6c9ddf617e5489b8b827900587422c8f
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-07-08 07:30:35 -0300

    modified

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,18 @@
 import "./TodoItem.css"
 import cancel from "./icons/cancel.svg"
 import approval from "./icons/approval.svg"
+import cancel_active from "./icons/cancel_active.svg"
+// import approval_active from "./icons/approval_active.svg"
 
 function TodoItem(props){
+  const pepe = () => {
+    document.querySelector('#cancel_logo').src = cancel_active;
+  }
     return (
       <li className="item">
         <img src={cancel} alt='cancel logo' id='cancel_logo'/>
         <p>{props.text}</p>
-        <img src={approval} alt='approval logo' id='approval_logo'/>
+        <img src={approval} alt='approval logo' id='approval_logo' onMouseOver={pepe}/>
       </li>
     );
 }
