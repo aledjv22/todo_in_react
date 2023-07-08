9a9ee72820c96370d610a8ae3250ca35ef18c20e
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-07-08 09:14:38 -0300

    change logos when hovering

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,18 +1,40 @@
+import { useState } from 'react';
 import "./TodoItem.css"
 import cancel from "./icons/cancel.svg"
 import approval from "./icons/approval.svg"
 import cancel_active from "./icons/cancel_active.svg"
-// import approval_active from "./icons/approval_active.svg"
+import approval_active from "./icons/approval_active.svg"
 
 function TodoItem(props){
-  const pepe = () => {
-    document.querySelector('#cancel_logo').src = cancel_active;
-  }
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const [isHovered2, setIsHovered2] = useState(false);
+  const handleMouseEnter2 = () => {
+    setIsHovered2(true);
+  };
+  const handleMouseLeave2 = () => {
+    setIsHovered2(false);
+  };
+
     return (
       <li className="item">
-        <img src={cancel} alt='cancel logo' id='cancel_logo'/>
+        <img 
+        src={isHovered2 ? cancel_active : cancel} 
+        alt='cancel logo' id='cancel_logo'
+        onMouseEnter={handleMouseEnter2}
+        onMouseLeave={handleMouseLeave2}/>
         <p>{props.text}</p>
-        <img src={approval} alt='approval logo' id='approval_logo' onMouseOver={pepe}/>
+        <img 
+        src={isHovered ? approval_active : approval} 
+        alt='approval logo' id='approval_logo' 
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}/>
       </li>
     );
 }
