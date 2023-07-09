640c35b39641421745c304c4ab6c096cf0afee10
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-07-09 13:49:43 -0300

    pointer interaction with button

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import './Button.css'
 import add from "./icons/add.svg"
+import add_active from "./icons/add_active.svg"
 
 function Button(){
+    const [isHovered, setIsHovered] = useState(false);
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
     return (
-        <button className="add-button">
-            <img src={add} alt="add to-do"/>  
+        <button className="add-button"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
+            <img src={isHovered ? add_active : add}
+            alt="add to-do"/>  
         </button>
     );
 }
