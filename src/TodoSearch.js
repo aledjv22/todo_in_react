ee82d416f55aef9d33f53989db04753df7ffdbe6
Victor Alejandro Diaz Jauregui <victor.diaz.jauregui@mi.unc.edu.ar>
2023-07-09 21:27:42 -0300

    read search status

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import "./TodoSearch.css"
 
 function TodoSearch(){
+    const [isValue, setIsValue] = useState('');
+    const value = (event) => {
+        const element = event.target.value;
+        setIsValue(element);
+    }
+    console.log(isValue);
     return (
-        <input placeholder="Cut onion" id="search"/>
+        <input placeholder="Cut onion" id="search" onChange={value}/>
+        
     );
 }
 
